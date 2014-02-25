afb7ed6e95709c3b716103943db07b7792e91e2a
alexford <alex@cratr.co>
2014-02-25 10:55:07 -0500

    Working on Twilio/Stripe integration

--- hirealexford.rb
+++ hirealexford.rb
@@ -1,6 +1,71 @@
+require 'json'
+require 'twilio-ruby'
+require 'stripe'
 require 'sinatra'
+
+
+## Stripe setup
+Stripe.api_key = ENV['STRIPE_KEY']
+Stripe.api_version = "2014-01-31"
+
+## log params
+before do
+  p params
+end
 
 ## landing page
 get '/' do
   haml :index
+end
+
+## form submit
+post '/reserve' do
+  content_type :json
+  @reservation = params[:reservation]
+  unless @reservation
+    # TODO better validation
+    error(422, 'No reservation data')
+  end
+
+  # attempt to create stripe customer
+  begin
+    customer = Stripe::Customer.create(
+      :description => "hirealexford.com consulting customer",
+      :email => @reservation[:email],
+      :card => @reservation[:token],
+      :metadata => {
+        'name' => @reservation[:name],
+        'message' => @reservation[:message]
+      }
+    )
+  rescue Stripe::StripeError
+    # oh noes! TODO make this error logging better
+    error(500, 'StripeError')
+  end
+
+  # notify me!
+  sendsms('New hirealexford.com customer: #{@reservation[:name]} (#{@reservation[:email]}). #{@reservation[:message]}')
+
+  # return Stripe customer data to frontend  
+  customer.to_json
+end
+
+get '/testmessage' do
+  sendsms('Test message')
+end
+
+#### SMS Helper
+def sendsms(message, to = ENV['TWILIO_TO_NUMBER'], from = ENV['TWILIO_FROM_NUMBER'])
+  begin
+    @twilio_client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    @twilio_client.account.messages.create(
+      :from => from,
+      :to => to,
+      :body => message
+    )
+  rescue Twilio::REST::RequestError => e
+    error(500, "TwilioError #{e.message}")
+  rescue
+    puts "Another error"
+  end
 end
